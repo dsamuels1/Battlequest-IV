d7c7aed4f5c873e414fc5155314f795a2a619a15
weilerm3 <weilerm3@udayton.edu>
2017-04-25 18:05:34 -0400

    Here is the level up test and use potion test

--- test/battlequest/iv/BattlequestIVTest.java
+++ test/battlequest/iv/BattlequestIVTest.java
@@ -23,10 +23,10 @@ public class BattlequestIVTest {
     
     @Test
     public void HeroAttackTest(){
-        Hero mew = new Hero(5,5,30,5,5000);
-        Enemy mewTwo = new Enemy(5,5,500,5);
-        mewTwo.attack(mew);
-        assertEquals(mew.getHp(), 4998);
+        Hero mew = new Hero(250,300,30,50,5000);
+        Enemy mewTwo = new Enemy(275,250,50,75,1250);
+        mew.attack(mewTwo);
+        assertEquals(5000 - 480, mew.getHp());
     }
     
      @Test
@@ -36,12 +36,16 @@ public class BattlequestIVTest {
     
     @Test
     public void levelUpTest(){
-        
+        Hero Bob = new Hero(20, 20, 20, 2, 5000);
+		Bob.levelUp();
+		assertEquals(3, Bob.getLevel());
     }
     
     @Test
     public void usePotionTest(){
-        
+        Hero Bob = new Hero(20, 20, 20, 2, 5000, 1);
+        Bob.usePotion();
+	assertEquals(5200, Bob.getHp());
     }
     
     @Test
